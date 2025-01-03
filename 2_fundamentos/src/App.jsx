025c457162e5dfe380932533beca4d74c7956de9
Heberton Geovane <heberton.geovane@gmail.com>
2025-01-03 08:52:42 -0300

    feat(components): add Welcome.jsx component

---
+++ 2_fundamentos/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Welcome from './components/Welcome'
+
+
+function App() {
+ 
+  return (
+    <>
+    {/* 6.1 - Criação de componente */}
+    <Welcome />
+    </>
+  )
+}
+
+export default App
---
+++ 2_fundamentos/src/components/Welcome.jsx
@@ -0,0 +1,6 @@
+function Welcome () {
+     
+    return <h1>Olá mundo</h1>
+}
+
+export default Welcome;
