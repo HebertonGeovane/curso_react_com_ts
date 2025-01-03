a65613d4025b81df345b5456752ad0ba82a2921d
Heberton Geovane <heberton.geovane@gmail.com>
2025-01-03 10:37:21 -0300

    feat: add Cachorro.jsx

--- 2_fundamentos/src/App.jsx
+++ 2_fundamentos/src/App.jsx
@@ -1,8 +1,9 @@
 import './App.css'
 import BomDia from './components/BomDia'
 import Welcome from './components/Welcome'
-import Pai from './components/Pai';
-import Descricao from './components/Descricao';
+import Pai from './components/Pai'
+import Descricao from './components/Descricao'
+import Cachorro from './components/Cachorro'
 function App() {
  
   return (
@@ -15,6 +16,8 @@ function App() {
     <Pai />
     {/* 6.4 Props */}
     <Descricao nome = "Heberton" idade={39} />
+    {/* 6.5 Desestruturação de props */}
+    <Cachorro nome = "Spike" raca= "Caramelo" />
     </>
   )
 }
---
+++ 2_fundamentos/src/components/Cachorro.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Cachorro = ({nome , raca}) => {
+  return (
+    <div>
+        <p>
+            O cachorro tem o nome de {nome}  e sua raça é {raca}
+        </p>
+    </div>
+  )
+}
+
+export default Cachorro
